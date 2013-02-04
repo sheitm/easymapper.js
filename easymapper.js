414b31de7a7854769610bd8fdbd76c0c3cb24c11
Ståle Heitmann <ssheitmann@gmail.com>
2013-02-04 20:10:32 +0100

    DIv

---
+++ easymapper.js
@@ -0,0 +1,13 @@
+(function(global){
+	var mapFromServerFunc = function(src, dest) {
+		var self = this;
+
+		for(var srcProperty in src) {
+			
+		}
+	};
+
+	global.easymapper = {
+		mapFromServer: mapFromServerFunc
+	};
+})(this);
---
+++ test/core.js
@@ -0,0 +1,7 @@
+/// <reference path="./vendor/qunit.js"/>
+
+module("Core");
+
+test("My first test", function(){
+	equal(3, 2+1);
+});
